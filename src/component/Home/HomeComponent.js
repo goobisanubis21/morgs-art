96892d3f2e1f48566cb426dca4337433e43371f7
Anthony Parrino <anthony21parrino21@gmail.com>
2021-02-07 12:11:59 -0500

    changed background color

--- src/component/Home/HomeComponent.js
+++ src/component/Home/HomeComponent.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import "./homeComponent.css";
+import genisis from "./genisis.JPG";
 
 
 function Home() {
 
     return (
-        <div>
-            home page
+        <div id="homePage">
+            <div id= "featuredDiv">
+                <h2>Featured Painting</h2>
+                <img id="featuredPainting" alt="genisis" src={genisis}></img>
+            </div>
         </div>
     )
 }
