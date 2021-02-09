69a6127dd13899a19c4a2eb072927901eb5b9050
Anthony Parrino <anthony21parrino21@gmail.com>
2021-02-08 22:52:14 -0500

    added photos to gallery

--- src/component/Gallery/GalleryComponent.js
+++ src/component/Gallery/GalleryComponent.js
@@ -5,7 +5,7 @@ function GalleryComponent(props) {
     return (
         <div id="gallery">
             <h3>{props.title}</h3>
-            <p><img src= {props.image} alt={props.title}></img></p>
+            <p><img id="galleryImg" src= {props.image} alt={props.title}></img></p>
             <p>{props.price}</p>
             <p>{props.date}</p>
             <button id="btn" type="submit">Add to Cart</button>
