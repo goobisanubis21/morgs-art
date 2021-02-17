20ddc0f38a52d06030fb78486af7b32384651152
Anthony Parrino <anthony21parrino21@gmail.com>
2021-02-17 12:39:50 -0500

    added about me info and featured paitning info

--- client/src/component/AboutMe/AboutMeComponent.js
+++ client/src/component/AboutMe/AboutMeComponent.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import "./about.css";
+import Morgs from "./morgs.png";
 
 function AboutMeComponent() {
     return (
         <div>
-            about me component
+            <div className="aboutImgDiv">
+                <h2 className="aboutArtistText">About the Artist</h2>
+                <img className="morgs" src={Morgs} alt="Morgs"></img> 
+            </div>
+            <div className="aboutMeInfo card">
+                Born in New Jersey, Morgan Danton has been oil painting for the past five years, but her skills far exceed that short amount of time with the brush. Being fascinated with art since the day she was born, Morgan always knew fate would eventually lead her to the life long journey of mastering oils. Since then she has done comissioned work, sold originals, designed tattoos for clients and has done mulitple art shows all over the state. Her photo realism portraits are one of her highlighting factors in her unique style, along with a specific color pallete for each individual piece, they are the center of attention in any room. Morgan's inspirtion is typically not drawn from any other person in the world of art but rather nature and culture.
+            </div>
         </div>
     )
 }
--- client/src/component/Home/HomeComponent.js
+++ client/src/component/Home/HomeComponent.js
@@ -11,7 +11,8 @@ function Home() {
                 <h1 className="featuredPaintingText">Featured Painting</h1>
                 <img id="featuredPainting" alt="genisis" src={genisis}></img>
                 <h3>Genisis</h3>
-                <h4>painting info</h4>
+                <h4>24" x 48"</h4>
+                <h4 className="featuredInfo">Morgan's current featured painting Genisis is a mixed media style portrait made with oils on canvas along with book pages giving it a very unique style.</h4>
             </div>
         </div>
     )
